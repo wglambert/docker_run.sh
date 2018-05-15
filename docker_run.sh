f18313105d2723d5d230a082fc45ef1af8d08c59
wglambert <W.lambert1031@gmail.com>
2018-05-15 16:40:04 -0700

    Create docker_run.sh

---
+++ docker_run.sh
@@ -0,0 +1,28 @@
+image=$(echo "$1" | cut -d: -f1 | cut -d- -f1)
+if [ -z "$(docker ps | grep $image)" ]; then
+        image_num=$image
+else
+        image_num=$(echo -e "$image$(docker ps | grep $image | wc -l)")
+fi
+
+echo "options; defaults to -it"
+   read args
+      if [ -z "$args" ]; then \
+        args="-it"
+      fi
+echo "$image_num"
+      echo "Run entrypoint and in background, y/n?"
+        read answer
+        case $answer in
+                y | yes | "") \
+                  docker run --rm $args --name $image_num -dit $1 && \
+                  echo -e "$ docker run --rm $args --name $image_num -dit $1
+$ docker exec -it $image_num bash" &&
+#                 sleep 1 &&
+                  docker exec -it $image_num bash ;;
+
+                n | no) \
+                  echo -e "docker run --rm $args --name $image_num -it $1 bash" &&
+                  docker run --rm $args --name $image_num -it $1 bash ;;
+        esac
+          }
