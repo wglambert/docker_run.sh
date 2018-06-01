047ed1242c97d5264b8bb84a98362754298cf0c3
wglambert <W.lambert1031@gmail.com>
2018-06-01 15:39:33 -0700

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -2,7 +2,13 @@ image=$(echo "$1" | cut -d: -f1 | cut -d- -f1)
 if [ -z "$(docker ps | grep $image)" ]; then
         image_num=$image
 else
-        image_num=$(echo -e "$image$(docker ps | grep $image | wc -l)")
+        image_num=$(echo -e "$image$(docker ps | grep $image | wc -l)") 
+fi
+
+if [ "$(echo "$1" | grep alpine)" ]; then
+        shell=sh
+else
+        shell=bash
 fi
 
 echo "options; defaults to -d"
@@ -10,15 +16,15 @@ echo "options; defaults to -d"
       if [ -z "$args" ]; then \
         args="-d"
       fi
-echo "$image_num"
-      echo "Run entrypoint and run in background, else foreground, y/n?"
+echo -e "container name: $image_num\n"
+      echo "Exec -it $image_num $shell, y/n?"
         read -e answer
         case $answer in
                 y | yes | "") \
                   docker run --rm $args --name $image_num -d $1 && \
                   echo -e "$ docker run --rm $args --name $image_num $1
-$ docker exec -it $image_num bash" && 
-                  docker exec -it $image_num bash ;; 
+$ docker exec -it $image_num $shell" && 
+                  docker exec -it $image_num $shell ;; 
 
                 n | no) \
                   echo -e "docker run --rm $args --name $image_num -it $1" &&
