8e9e0576f5049864a0bc9d099749df43675191b6
wglambert <W.lambert1031@gmail.com>
2018-06-01 16:38:43 -0700

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -16,17 +16,17 @@ echo "options; defaults to -d"
       if [ -z "$args" ]; then \
         args="-d"
       fi
-echo -e "container name: $image_num\n"
+echo -e "container name: \033[0;36m$image_num\033[0m \n"
       echo "Exec -it $image_num $shell, y/n?"
         read -e answer
         case $answer in
                 y | yes | "") \
                   docker run --rm $args --name $image_num -d $1 && \
-                  echo -e "$ docker run --rm $args --name $image_num $1
+                  echo -e "\n$ docker run --rm $args --name $image_num $1
 $ docker exec -it $image_num $shell" && 
                   docker exec -it $image_num $shell ;; 
 
                 n | no) \
-                  echo -e "docker run --rm $args --name $image_num -it $1" &&
+                  echo -e "\n$ docker run --rm $args --name $image_num -it $1" &&
                   docker run --rm $args --name $image_num -it $1 ;;
         esac
