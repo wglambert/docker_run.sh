cbb016e5144ef09ddead698921c24476bdb0278d
wglambert <W.lambert1031@gmail.com>
2018-10-05 14:53:04 -0700

    Add readline history

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,6 @@
-image=$(echo "$1" | cut -d- -f1 | cut -d: -f1 | cut -d- -f1 | cut -d/ -f2)
+#!/bin/bash
+
+image=$(echo "$1" | cut -d- -f1 | cut -d: -f1 | cut -d/ -f2)
 if [ -z "$(docker ps | grep $image)" ]; then
         image_num=$image
 else
@@ -11,22 +13,28 @@ else
         shell=bash
 fi
 
-echo "options; defaults to -d"
-   read -e args
-      if [ -z "$args" ]; then \
+echo "options; if empty defaults to -d; --rm is implicit"
+
+#https://superuser.com/a/710244
+args=$(HISTFILE=~/."${0##*/}"_args_history;
+                history -r; read -e args;
+                history -s -- "$args"; history -w;
+                echo "$args")
+
+if [ -z "$args" ]; then \
         args="-d"
       fi
 echo -e "container name: \033[0;36m$image_num\033[0m \n"
       echo "exec -it $image_num $shell, y/n?"
-        read -e answer
+      read -e answer
         case $answer in
                 y | yes | "") \
                   docker run --rm $args --name $image_num -d $1 && \
                   echo -e "\n$ docker run --rm $args --name $image_num $1
 $ docker exec -it $image_num $shell" && 
-                  docker exec -it $image_num $shell ;; 
+                  docker exec -it $image_num $shell ;;
 
-                n | no) \
+                          n | no) \
                   echo -e "\n$ docker run --rm $args --name $image_num -it $1" &&
                   docker run --rm $args --name $image_num -it $1 ;;
         esac
